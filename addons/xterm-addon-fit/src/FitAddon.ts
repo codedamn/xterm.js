53883d6b3408c2878f81af34074d816ea3089a12
Mehul Mohan <mehulmpt@gmail.com>
2020-06-24 22:01:28 +0530

    fix: isFinite check on rows/cols in xterm-addon-fit

--- addons/xterm-addon-fit/src/FitAddon.ts
+++ addons/xterm-addon-fit/src/FitAddon.ts
@@ -3,7 +3,7 @@
  * @license MIT
  */
 
-import { Terminal, ITerminalAddon } from 'xterm';
+import { Terminal, ITerminalAddon } from "xterm";
 
 interface ITerminalDimensions {
   /**
@@ -60,22 +60,26 @@ export class FitAddon implements ITerminalAddon {
     const core = (this._terminal as any)._core;
 
     const parentElementStyle = window.getComputedStyle(this._terminal.element.parentElement);
-    const parentElementHeight = parseInt(parentElementStyle.getPropertyValue('height'));
-    const parentElementWidth = Math.max(0, parseInt(parentElementStyle.getPropertyValue('width')));
+    const parentElementHeight = parseInt(parentElementStyle.getPropertyValue("height"));
+    const parentElementWidth = Math.max(0, parseInt(parentElementStyle.getPropertyValue("width")));
     const elementStyle = window.getComputedStyle(this._terminal.element);
     const elementPadding = {
-      top: parseInt(elementStyle.getPropertyValue('padding-top')),
-      bottom: parseInt(elementStyle.getPropertyValue('padding-bottom')),
-      right: parseInt(elementStyle.getPropertyValue('padding-right')),
-      left: parseInt(elementStyle.getPropertyValue('padding-left'))
+      top: parseInt(elementStyle.getPropertyValue("padding-top")),
+      bottom: parseInt(elementStyle.getPropertyValue("padding-bottom")),
+      right: parseInt(elementStyle.getPropertyValue("padding-right")),
+      left: parseInt(elementStyle.getPropertyValue("padding-left")),
     };
     const elementPaddingVer = elementPadding.top + elementPadding.bottom;
     const elementPaddingHor = elementPadding.right + elementPadding.left;
     const availableHeight = parentElementHeight - elementPaddingVer;
     const availableWidth = parentElementWidth - elementPaddingHor - core.viewport.scrollBarWidth;
+
+    const cols = Math.floor(availableWidth / core._renderService.dimensions.actualCellWidth);
+    const rows = Math.floor(availableHeight / core._renderService.dimensions.actualCellHeight);
+
     const geometry = {
-      cols: Math.max(MINIMUM_COLS, Math.floor(availableWidth / core._renderService.dimensions.actualCellWidth)),
-      rows: Math.max(MINIMUM_ROWS, Math.floor(availableHeight / core._renderService.dimensions.actualCellHeight))
+      cols: Math.max(MINIMUM_COLS, isFinite(cols) ? cols : 0),
+      rows: Math.max(MINIMUM_ROWS, isFinite(rows) ? rows : 0),
     };
     return geometry;
   }
